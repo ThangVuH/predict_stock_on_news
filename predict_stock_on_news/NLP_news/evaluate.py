a41527d60f10431d9baea116b15fad4a89fbc078
Thang VuH <thang.vu.working@gmail.com>
2022-09-06 10:50:36 +0200

    return y_pred from fit_model() func

--- predict_stock_on_news/NLP_news/evaluate.py
+++ predict_stock_on_news/NLP_news/evaluate.py
@@ -4,5 +4,7 @@ from tqdm.auto import tqdm
 accuracys = []
 
 for name,clf in tqdm(clfs.items()):
-    curr_acc = fit_model(clf,x_train,y_train,x_test,y_test)
+    #curr_acc = fit_model(clf,x_train,y_train,x_test,y_test)
+    y_pred = fit_model(clf,x_train,y_train,x_test,y_test)
+    curr_acc = accuracy_score(y_pred, y_test)
     accuracys.append(curr_acc)
--- predict_stock_on_news/NLP_news/model.py
+++ predict_stock_on_news/NLP_news/model.py
@@ -38,5 +38,5 @@ clfs = {
 def fit_model(clf,x_train,y_train,x_test, y_test):
     clf.fit(x_train,y_train)
     y_pred = clf.predict(x_test)
-    accuracy = accuracy_score(y_pred, y_test)
-    return accuracy
+    #accuracy = accuracy_score(y_pred, y_test)
+    return y_pred
