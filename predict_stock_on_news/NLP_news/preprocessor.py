6bfa2ff8ad973d9c2de1bfe09e72e6ef9b375958
Thang VuH <thang.vu.working@gmail.com>
2022-09-06 17:12:32 +0200

    more function for preprocess

--- predict_stock_on_news/NLP_news/preprocessor.py
+++ predict_stock_on_news/NLP_news/preprocessor.py
@@ -2,10 +2,12 @@ import string
 import pandas as pd
 import numpy as np
 import re
+from collections import  Counter
 
 # nltk
 import nltk
 nltk.download('stopwords')
+from nltk.stem import WordNetLemmatizer,PorterStemmer
 from nltk.corpus import stopwords
 
 
@@ -22,3 +24,18 @@ def basic_cleaning(sentence):
     sentence = ' '.join([word for word in sentence.split() if word.lower() not in STOPWORDS])
 
     return sentence
+
+def tokenizing_text(text):
+  new= text.str.split()
+  new=new.values.tolist()
+  return new
+
+def stem_and_lem(text):
+  corpus=[]
+  stem=PorterStemmer()
+  lem=WordNetLemmatizer()
+  for i in text:
+    words=[lem.lemmatize(w) for w in i if len(w)>2]
+    words=[stem.stem(w) for w in i if len(w)>2]
+    corpus.append(words)
+  return corpus
