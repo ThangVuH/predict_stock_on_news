cf77120f0ac8aa66cf0c90a0049d5566b7201048
laura martel <lpm.martel@gmail.com>
2022-09-06 17:41:51 +0200

    Model SP500 Fbprophet

---
+++ predict_stock_on_news/model.py
@@ -0,0 +1,70 @@
+# Time series with FBprophet
+
+### Packages and libraries to install
+##General libraries
+
+import pandas as pd
+import plotly.express as px
+from statsmodels.tools.eval_measures import rmse
+
+
+# Facebook specific
+# pip install pystan~=2.14  #This specific version of pystan must be installed to have fbprophet
+#pip install fbprophet
+
+
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly, plot_components_plotly
+from prophet.diagnostics import cross_validation
+from prophet.diagnostics import performance_metrics
+
+import sys
+sys.path.insert(1, "data/SP500_predict/")
+import data
+from data import df_main
+
+
+data = df_main()
+
+## Choose your data by installing yahoo finance for example
+# Enter a dataframe with columns 'ds' for date in format yyyy-mm-dd and 'y' for the data
+# Tip, it is better to run the model on a 4 years time
+
+
+def data_processing(self):
+    self = self.reset_index()
+    df = self[['Date', 'Close']]
+    df.columns = ['ds', 'y']
+    return df
+
+def fit_model(self):
+    """### Creating and Training the model
+    You fit the model ***on all the dataset *** it is like this in FBprophet
+    """
+    fbp = Prophet() # model named fbp
+    fbp.add_seasonality(name='monthly', period=30.5, fourier_order=5)
+    model = fbp.fit(self)
+    return model
+
+# Test your model
+def test_model(self, data):
+    test = data[round(len(data)*0.8):]
+    forecast = self.predict(test)
+    predictions = forecast.iloc[-len(test):]['yhat']
+    actuals = test['y']
+    return f"RMSE: {round(rmse(predictions, actuals))}"
+
+# Predict the future
+def forecasting(self):
+  future = self.make_future_dataframe(periods=60)
+  return self.predict(future)
+
+# Plot your graph (model and prediction)
+def plot_forecast(model, prediction):
+  return plot_plotly(model, prediction)
+
+p = data_processing(data)
+m = fit_model(p)
+t = test_model(m, p)
+f = forecasting(m)
+print(f)
