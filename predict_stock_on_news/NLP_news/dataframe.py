18f086068df1f26bd38d2a23f46ae422795fd45e
Thang VuH <thang.vu.working@gmail.com>
2022-09-07 12:39:22 +0200

    done

--- predict_stock_on_news/NLP_news/dataframe.py
+++ predict_stock_on_news/NLP_news/dataframe.py
@@ -1,4 +1,4 @@
-from NLP_news.preprocessor import basic_cleaning
+from NLP_news.preprocessor import basic_cleaning, tokenizing_text
 from NLP_news.polar_subj import get_subjectivity, get_polarity
 from NLP_news.sentiment import sentiment_score, label_sentiment
 import pandas as pd
@@ -6,6 +6,7 @@ import numpy as np
 import plotly.express as px
 import seaborn as sns
 from wordcloud import WordCloud, STOPWORDS
+from collections import  Counter
 import matplotlib.pyplot as plt
 # split dataset
 from sklearn.model_selection import train_test_split
@@ -43,31 +44,27 @@ def create_dataframe():
 
     return data_cleaned
 
-
 def plot_dataframe():
     df_headlines = create_dataframe()
     def plot_dataframe1():
         fig_1 = px.scatter(df_headlines, x = create_dataframe.index , y ='compound', color = 'Sentiment' )
         return fig_1.show()
-    def plot_dataframe2():
-        fig_2 =px.scatter(df_headlines, x = 'Polarity' , y ='Subjectivity', color = 'compound')
-        return fig_2.show()
-    def plot_dataframe3():
-        fig_3, ax = plt.subplots(figsize=(8, 8))
-
-        counts = df_headlines['Sentiment'].value_counts(normalize=True) * 100
-
-        sns.barplot(x=counts.index, y=counts, ax=ax)
-
-        ax.set_xticklabels(['Negative', 'Neutral', 'Positive'])
-        ax.set_ylabel("Percentage")
-
-        plt.show()
-        return fig_3.show()
     def plot_dataframe4():
         fig_4 =px.scatter(df_headlines, x = df_headlines.index , y ='compound', color = 'Label', color_continuous_scale='portland')
         return fig_4.show()
 
+    def plot_dataframe2():
+        fig_2 =px.scatter(df_headlines, x = 'Polarity' , y ='Subjectivity', color = 'compound')
+        return fig_2.show()
+    def plot_dataframe3():
+        count1 = df_headlines['Sentiment'].value_counts()
+        dff = pd.DataFrame()
+        dff['name']= [str(i)for i in count1.index]
+        dff['number'] = count1.values
+        fig_3 = px.pie(dff, values="number", names="name")
+        return fig_3.show()
+
+# no need this show_wordcloud() function
 def show_wordcloud(data, title = None):
     wordcloud = WordCloud(
         background_color='white',
@@ -86,6 +83,20 @@ def show_wordcloud(data, title = None):
 
     plt.imshow(wordcloud)
     plt.show()
+
+def plot_common_word(text):
+  new =tokenizing_text(text)
+  corpus=[word for i in new for word in i]
+  counter=Counter(corpus)
+  most=counter.most_common()
+  x, y=[], []
+  for word,count in most[:40]:
+    x.append(word)
+    y.append(count)
+
+  fig =px.bar(text,x=y,y=x)
+  return fig.show()
+
 
 def split_data():
     df_headlines = create_dataframe()
