46d443f5520c211657566a95f8d3c199fa7f975e
laura martel <lpm.martel@gmail.com>
2022-09-08 23:29:29 +0200

    mise en forme

--- predict_stock_on_news/TimeSeries/streamlite/app.py
+++ predict_stock_on_news/TimeSeries/streamlite/app.py
@@ -1,10 +1,11 @@
+from turtle import width
 import streamlit as st
 import numpy as np
 import pandas as pd
 import datetime
 import plotly.graph_objects as go
-
-
+from pathlib import Path
+import base64
 import sys
 sys.path.insert(1, "../../data/SP500_predict/")
 import data
@@ -12,20 +13,54 @@ from data import df_main
 data = df_main()
 
 
-sys.path.insert(2, "../")
+sys.path.insert(1, "../")
 import model
 from model import data_processing
 from model import fit_model
 from model import test_model
 from model import forecasting
 
+# sys.path.insert(1, "../../NLP_news")
+# import final
+# from final import create_dataframe
+
+data_nyt= pd.read_csv("../../NLP_news/dataset_NYTimes.csv")
+
+#data_nyt2= create_dataframe()
 
 
 st.set_page_config(
-    page_title="NLP predict"
+    page_title="NLP predict",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+    img_to_bytes("VuYT.png")
 )
 
-st.title('Prediction of Time Series')
+col1, col2, col3 = st.columns([1,2,20])
+with col3:
+    st.markdown(
+        header_html, unsafe_allow_html=True,
+    )
+
+    #st.title('Vu make$ mooney B!TCH')
+
+
+
+
+
+with st.sidebar:
+
+    datum = st.date_input("Pick a date:", datetime.date(2020,7,6),
+                      min_value= datetime.date(2019,7,6),
+                      max_value=datetime.date(2022,10,2))
+    st.write(f"This is the market prediction for the week following {datum}")
 
 data = df_main()
 
@@ -39,23 +74,39 @@ t = test_model(m, p)
 #### Forecast the future
 f = forecasting(m)
 ##### Plot forecast and data
+
+
 def binar(df):
     df['Target'] = np.where(df['yhat'].shift(-1) > df['yhat'], 1, 0)
     return df
 
+
+
 dataf = binar(f)
 
+
+
+datee = datum.strftime("%Y-%m-%d")
+#st.write(datee)
+
+
+start_date = datee
+nyt_text = data_nyt.loc[data_nyt['Date'] == datee]['News']
+
+
+st.markdown("------")
+st.header(nyt_text.values)
+
+
+
 merged_df = dataf.merge(p, how='left', on='ds')
+
+
 
 #start_date = st.date_input("Start Date", value=pd.to_datetime("2021-01-31", format="%Y-%m-%d"))
 #end_date = st.date_input("End Date", value=pd.to_datetime("today", format="%Y-%m-%d"))
 #start_date = datee
-datum = st.date_input("Start Date", datetime.date(2020,7,6),
-                      min_value= datetime.date(2019,7,6),
-                      max_value=datetime.date(2022,10,2))
-datee = datum.strftime("%Y-%m-%d")
-st.write(datee)
-start_date = datee
+
 
 
 
@@ -96,7 +147,7 @@ def graph_daterange(df, start_date):
                               y=df['yhat_upper'],
                               name='yhat_upper',
                               mode='lines',
-                              line=dict(color='blue', width=3)
+                              line=dict(color='orange', width=3)
                               ))
 
     # plot lower CI
@@ -105,7 +156,7 @@ def graph_daterange(df, start_date):
                               name='yhat_lower',
                               mode='lines',
                               fill='tonexty',
-                              line=dict(color='blue', width=3)
+                              line=dict(color='orange', width=3)
                               ))
 
     grph.update_layout(title="Stock Market",
@@ -116,6 +167,7 @@ def graph_daterange(df, start_date):
 figu = graph_daterange(merged_df, start_date)
 
 st.plotly_chart(figu, use_container_width=True)
+figu.update_layout(width=30, height=20)
 
 # #st.table(f)
 # trend = f.loc[f.ds == start_date]["Target"]
